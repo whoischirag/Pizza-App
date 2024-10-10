1a0f257f602981b739e6a3c5784819dc1158f8f6
chirag <chiragsharma000903@gmail.com>
2024-10-10 16:37:52 +0530

    made login and register page

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-const express= require('express')
-const port=process.env.PORT || 3000
-const app= express()
-const ejs = require('ejs')
-const expressLayout = require('express-ejs-layouts')
-const path =require('path')
+const express = require("express");
+const port = process.env.PORT || 3000;
+const app = express();
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
 
 //assets
-app.use(express.static('public'))
+app.use(express.static("public"));
+
+//setting template engine
+const viewsPath = path.join(__dirname, "/resources/views");
+
+app.use(expressLayout);
+app.set("views", viewsPath);
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 
-app.get('/',(req,res)=>{
-res.render('home')
-
-})
-
-//setting template engine 
-const viewsPath= path.join(__dirname,'/resources/views')
-
-app.use(expressLayout)
-app.set("views",viewsPath)
-app.set('view engine', 'ejs')
-
-app.listen(port,()=>{
-    console.log(`Server is up and running on port ${port}`);
-
-})
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+});
